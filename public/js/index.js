b2e2e44ffd846e5846d8f5aa167b3693d291ca84
maxFritzen <max.fritzen@hotmail.se>
2019-02-02 19:07:29 +0100

    add mustache for template rendering

--- public/js/index.js
+++ public/js/index.js
@@ -11,27 +11,55 @@ socket.on('disconnect', function () {
 socket.on('newMessage', function (msg) {
   console.log('New msg', msg);
   var formattedTime = moment(msg.createdAt).format('h:mm');
-  var ul = document.getElementById('messages');
-  var li = document.createElement('li');
-  var text = document.createTextNode(`${msg.from} ${formattedTime}: ${msg.text}`);
-  li.appendChild(text);
-  ul.appendChild(li);
+  var template = document.getElementById('message-template').innerHTML;
+  var html = Mustache.render(template, {
+    text: msg.text,
+    from: msg.from,
+    createdAt: formattedTime
+  });
+
+  var messages = document.getElementById('messages');
+  var test = document.createElement('li');
+  test.innerHTML = html;
+  messages.appendChild(test);
+  // test.insertAdjacentText('beforeend', msg.text)
+  // messages.appendChild(test);
+  // var formattedTime = moment(msg.createdAt).format('h:mm');
+
+  // var messages = document.getElementById('messages');
+  // var li = document.createElement('li');
+  // var text = document.createTextNode(`${msg.from} ${formattedTime}: ${msg.text}`);
+  // li.appendChild(text);
+  // ul.appendChild(li);
 });
 
 socket.on('newLocationMessage', function (msg) {
-  console.log(msg.url);
+  console.log(msg);
+
   var formattedTime = moment(msg.createdAt).format('h:mm');
-  var ul = document.getElementById('messages');
-  var li = document.createElement('li');
-  var a = document.createElement('a');
-  a.href = msg.url;
-  a.target = '_blank';
-  a.textContent = 'My current location';
-  var text = document.createTextNode(`${msg.from} ${formattedTime}: `)
+  var template = document.getElementById('location-message-template').innerHTML;
+  var html = Mustache.render(template, {
+    url: msg.url,
+    from: msg.from,
+    createdAt: formattedTime
+  });
+
+  var messages = document.getElementById('messages');
+  var test = document.createElement('li');
+  test.innerHTML = html;
+  messages.appendChild(test);
+  // var formattedTime = moment(msg.createdAt).format('h:mm');
+  // var ul = document.getElementById('messages');
+  // var li = document.createElement('li');
+  // var a = document.createElement('a');
+  // a.href = msg.url;
+  // a.target = '_blank';
+  // a.textContent = 'My current location';
+  // var text = document.createTextNode(`${msg.from} ${formattedTime}: `)
   
-  li.appendChild(text);
-  li.appendChild(a);
-  ul.appendChild(li);
+  // li.appendChild(text);
+  // li.appendChild(a);
+  // ul.appendChild(li);
 });
 
 var locationButton = document.getElementById('send-location');
